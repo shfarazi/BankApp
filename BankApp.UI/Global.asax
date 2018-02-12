280078afa3a2b16e6319928661a3f42d7a44c641
Shannon Farazi <shannonfarazi@gmail.com>
2018-02-11 20:20:16 -0800

    MVC

---
+++ BankApp.UI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BankApp.UI.MvcApplication" Language="C#" %>
---
+++ BankApp.UI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BankApp.UI.Startup))]
+namespace BankApp.UI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
